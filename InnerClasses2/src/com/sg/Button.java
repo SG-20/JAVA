25e183c6f00171e18fbdc9bd50d930d10117c177
SG-20 <59667884+SG-20@users.noreply.github.com>
2020-03-01 16:26:22 +1300

    Add files via upload

---
+++ InnerClasses2/src/com/sg/Button.java
@@ -0,0 +1,27 @@
+package com.sg;
+
+public class Button {
+
+    private String title;
+    private OnClickListener onClickLister;
+
+    public Button(String title){
+        this.title=title;
+    }
+
+    public String getTitle(){
+        return title;
+    }
+
+    public void setOnClickLister(OnClickListener onClickLister){
+        this.onClickLister=onClickLister;
+    }
+
+    public void onClick(){
+        this.onClickLister.onClick(this.title);
+    }
+
+    public interface OnClickListener{
+        public void onClick(String title);
+    }
+}
