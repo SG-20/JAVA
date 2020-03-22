6bf48c9ebaa40566f547c13ecd72c1d77698bd08
SG-20 <59667884+SG-20@users.noreply.github.com>
2020-03-22 14:13:28 +1300

    Add files via upload

---
+++ Abstraction/src/com/sg/Main.java
@@ -0,0 +1,18 @@
+package com.sg;
+
+public class Main {
+
+    public static void main(String[] args){
+        Dog dog= new Dog("Yorkie");
+        dog.breathe();
+        dog.eat();
+
+        Parrot parrot = new Parrot("NZ Parrot");
+        parrot.breathe();
+        parrot.eat();
+        parrot.fly();
+
+        Penguin penguin= new Penguin("Emperor");
+        penguin.fly();
+    }
+}
